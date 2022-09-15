bd2e69bc37d46a42a53c42c3715237c4e76feb83
Vikash Kumar Roy <99469893+Vikashkrroy123@users.noreply.github.com>
2022-09-15 16:12:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Nav from './components/Nav';
+import Main from './components/Main'
+//import Emojylist from'./Emojylist'
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    <Main/>
+   </div>
+  );
+}
+
+export default App;
